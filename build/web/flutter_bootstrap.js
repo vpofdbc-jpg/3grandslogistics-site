5e68a1b1b8c5a14d74ae10633b32d9d2fe242462
Kelvin Council <vpofdbc@gmail.com>
2025-11-25 12:19:11 -0500

    Deploy: Updated Flutter web build artifacts

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"b9afb03275c7cf04906cb5b94faa60005561fc
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "369083666" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3094578884" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
